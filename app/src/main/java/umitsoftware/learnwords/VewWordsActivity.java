8e46f0dc0ec6cea76e2445e24c8c34bbe89f6fa0
mitjauly <mitjauly@gmail.com>
2017-01-28 23:28:46 +0300

    Rejected commit: Default

---
+++ app/src/main/java/umitsoftware/learnwords/VewWordsActivity.java
@@ -0,0 +1,13 @@
+package umitsoftware.learnwords;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VewWordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vew_words);
+    }
+}
